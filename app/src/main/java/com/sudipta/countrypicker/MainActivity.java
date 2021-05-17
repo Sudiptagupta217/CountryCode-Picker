9dc53b1f5277d02d10fad89ea02307acc17b1d51
Sudipta Gupta <sudiptagupta217@gmail.com>
2021-05-17 09:30:56 +0530

    added xml and java code

--- app/src/main/java/com/sudipta/countrypicker/MainActivity.java
+++ app/src/main/java/com/sudipta/countrypicker/MainActivity.java
@@ -3,12 +3,27 @@ package com.sudipta.countrypicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.rilixtech.widget.countrycodepicker.Country;
+import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
 
+    CountryCodePicker ccp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ccp = findViewById(R.id.ccp);
+
+        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected(Country selectedCountry) {
+                Toast.makeText(MainActivity.this, "country name "+selectedCountry.getName()+"," +
+                        " country code: "+selectedCountry.getPhoneCode(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
